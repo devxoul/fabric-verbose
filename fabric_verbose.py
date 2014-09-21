29e5ea568f6752d5f4f92c809011f4881b86ea3c
Suyeol Jeon <devxoul@gmail.com>
2014-09-22 03:36:06 +0900

    Import fabric modules first.

--- fabric_verbose.py
+++ fabric_verbose.py
@@ -16,36 +16,36 @@ __license__ = 'MIT'
 __copyright__ = '(c) 2014 by Suyeol Jeon'
 __all__ = ['verbose']
 
-import importlib
 import sys
+
+from fabric import api
+from fabric.colors import blue, red
 
 
 class verbose():
     def __init__(self, msg):
         self.msg = msg
-        self.operations = importlib.import_module('fabric.operations')
-        self.colors = importlib.import_module('fabric.colors')
         self.failed = False
         for func_name in ['local', 'run', 'sudo']:
             setattr(self, func_name, self._execute(func_name))
 
     def __enter__(self):
-        sys.stdout.write(self.colors.blue("* %s..." % self.msg))
+        sys.stdout.write(blue("* %s..." % self.msg))
         sys.stdout.flush()
         return self
 
     def __exit__(self, type, value, traceback):
         if not self.failed:
-            print self.colors.blue(" Done")
+            print blue(" Done")
 
     def _execute(self, func_name):
         def __execute(*args, **kwargs):
             if func_name == 'local' and 'capture' not in kwargs:
                 kwargs['capture'] = True
-            rv = getattr(self.operations, func_name)(*args, **kwargs)
+            rv = getattr(api, func_name)(*args, **kwargs)
             if rv.return_code:
                 self.failed = True
-                print self.colors.red(" Failed")
+                print red(" Failed")
                 abort_msg = rv.stderr.strip() or rv.stdout.strip()
-                self.operations.abort(self.colors.red(abort_msg))
+                api.abort(red(abort_msg))
         return __execute
